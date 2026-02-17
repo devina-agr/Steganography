ac3a6a2fa6387e84b9bd9ddce9eeafb896c3c602
devina-agr <devinaagrawal2005@gmail.com>
2026-02-17 14:41:48 +0530

    new commit

--- src/main/java/org/spring/steganography/Controller/UserController.java
+++ src/main/java/org/spring/steganography/Controller/UserController.java
@@ -1,10 +1,15 @@
 package org.spring.steganography.Controller;
 
+import org.spring.steganography.Model.User;
+import org.spring.steganography.Security.UserPrincipal;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+     public ResponseEntity<User> getAllUsers(@AuthenticationPrincipal UserPrincipal userPrincipal){}
 
 
 }
