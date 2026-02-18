deadafc843f5bcc4bcc2d2dec769e1bb5a4cad33
devina-agr <devinaagrawal2005@gmail.com>
2026-02-19 00:56:44 +0530

    new commit

--- src/main/java/org/spring/steganography/Util/KeyDerivationUtil.java
+++ src/main/java/org/spring/steganography/Util/KeyDerivationUtil.java
@@ -1,4 +1,31 @@
 package org.spring.steganography.Util;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+
 public class KeyDerivationUtil {
+
+    private static final int ITERATIONS=65536;
+    private static final int KEY_LENGTH=256;
+
+    public static byte[] generateSalt(){
+        byte[] salt=new byte[16];
+        new SecureRandom().nextBytes(salt);
+        return salt;
+    }
+
+    public static byte[] deriveKey(char[] password, byte[] salt){
+        try{
+            PBEKeySpec spec=new PBEKeySpec(password,salt,ITERATIONS,KEY_LENGTH);
+            SecretKeyFactory factory=SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            return factory.generateSecret(spec).getEncoded();
+        }
+        catch(Exception e){
+            throw new RuntimeException("Key derivation failed ",e);
+        }
+    }
+
+
 }
