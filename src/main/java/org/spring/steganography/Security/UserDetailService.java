1debae934146da05a9ca37f39ef0943e1cc2a23a
devina-agr <devinaagrawal2005@gmail.com>
2026-02-22 19:30:43 +0530

    new commit

--- src/main/java/org/spring/steganography/Security/UserDetailService.java
+++ src/main/java/org/spring/steganography/Security/UserDetailService.java
@@ -1,4 +1,25 @@
 package org.spring.steganography.Security;
 
-public class UserDetailService {
+import org.spring.steganography.Model.User;
+import org.spring.steganography.Repository.UserRepo;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+
+    public UserDetailService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user=userRepo.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("User not found!"));
+
+        return new UserPrincipal(user);
+    }
 }
