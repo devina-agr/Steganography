b295414b59f72994037f18a253fa039b249ccbf3
devina-agr <devinaagrawal2005@gmail.com>
2026-02-22 11:25:41 +0530

    new commit

---
+++ src/main/java/org/spring/steganography/Exception/ErrorResponse.java
@@ -0,0 +1,29 @@
+package org.spring.steganography.Exception;
+import java.time.LocalDateTime;
+
+
+public class ErrorResponse {
+
+    private final String message;
+    private final int status;
+    private final LocalDateTime timestamp;
+
+
+    public ErrorResponse(String message, int status, LocalDateTime timestamp) {
+        this.message = message;
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
--- src/main/java/org/spring/steganography/Exception/GlobalExceptionHandler.java
+++ src/main/java/org/spring/steganography/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,36 @@
 package org.spring.steganography.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ImageCapacityExceededException.class)
+    public ResponseEntity<ErrorResponse> handleImageCapacity(ImageCapacityExceededException e){
+        return buildResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidInviteException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidInvite(InvalidInviteException e){
+        return buildResponse(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<ErrorResponse> handleTokenExpired(TokenExpiredException e){
+        return buildResponse(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+
+
+
+    private ResponseEntity<ErrorResponse> buildResponse(String message, HttpStatus status) {
+        ErrorResponse errorResponse=new ErrorResponse(message, status.value(), LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse,status);
+    }
+
 }
