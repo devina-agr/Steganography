bbb610a2486b67a4d5a6adb3247b225e07f08876
devina-agr <devinaagrawal2005@gmail.com>
2026-02-22 18:35:37 +0530

    new commit

--- src/main/java/org/spring/steganography/Security/UserPrincipal.java
+++ src/main/java/org/spring/steganography/Security/UserPrincipal.java
@@ -1,4 +1,59 @@
 package org.spring.steganography.Security;
 
-public class UserPrincipal {
+import org.jspecify.annotations.Nullable;
+import org.spring.steganography.Model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRole()
+                .stream()
+                .map(role->new SimpleGrantedAuthority(role.name()))
+                .toList();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public String getUserId(){
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
