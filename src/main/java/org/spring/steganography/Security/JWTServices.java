21787fcd8c0093204b45a7ba6ea7a3bb2f9f11da
devina-agr <devinaagrawal2005@gmail.com>
2026-02-23 21:32:41 +0530

    new commit

--- src/main/java/org/spring/steganography/Security/JWTServices.java
+++ src/main/java/org/spring/steganography/Security/JWTServices.java
@@ -1,4 +1,23 @@
 package org.spring.steganography.Security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
 public class JWTServices {
+
+    private final String secret_key;
+    private final long expiration_value;
+
+    public JWTServices(@Value("jwt.secret") String secret_key,@Value("jwt.expiration") long expiration_value) {
+        this.secret_key = secret_key;
+        this.expiration_value = expiration_value;
+    }
+
+    private Key getSigningKey(){
+        return
+    }
+
 }
--- src/main/java/org/spring/steganography/Service/AdminService.java
+++ src/main/java/org/spring/steganography/Service/AdminService.java
@@ -1,4 +1,7 @@
 package org.spring.steganography.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
 }
