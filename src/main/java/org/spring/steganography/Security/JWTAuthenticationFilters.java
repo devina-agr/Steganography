1347252835db9ddd4fe48a7dd77fa6d96119f194
devina-agr <devinaagrawal2005@gmail.com>
2026-02-24 11:01:57 +0530

    new commit

--- src/main/java/org/spring/steganography/Security/JWTAuthenticationFilters.java
+++ src/main/java/org/spring/steganography/Security/JWTAuthenticationFilters.java
@@ -1,4 +1,29 @@
 package org.spring.steganography.Security;
 
-public class JWTAuthenticationFilters {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationFilters extends OncePerRequestFilter {
+
+    private final JWTServices jwtServices;
+    private final UserDetailService userDetailService;
+
+    public JWTAuthenticationFilters(JWTServices jwtServices, UserDetailService userDetailService) {
+        this.jwtServices = jwtServices;
+        this.userDetailService = userDetailService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader =request.getHeader("Authorization");
+
+    }
 }
