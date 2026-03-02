301a1547f13049d85c12c9d5fe33ceaa697db892
devina-agr <devinaagrawal2005@gmail.com>
2026-03-02 16:33:31 +0530

    first commit

--- src/main/java/org/spring/steganography/Controller/UserController.java
+++ src/main/java/org/spring/steganography/Controller/UserController.java
@@ -2,6 +2,7 @@ package org.spring.steganography.Controller;
 
 import org.spring.steganography.Model.User;
 import org.spring.steganography.Security.UserPrincipal;
+import org.spring.steganography.Service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-     public ResponseEntity<User> getAllUsers(@AuthenticationPrincipal UserPrincipal userPrincipal){
+    private UserService userService;
 
+     public ResponseEntity<User> getAllUsers(@AuthenticationPrincipal UserPrincipal userPrincipal){
+            return userService.getAllUsers(userPrincipal.getUserId());
      }
 
+     
 
 }
--- src/main/java/org/spring/steganography/Security/JWTAuthenticationFilters.java
+++ src/main/java/org/spring/steganography/Security/JWTAuthenticationFilters.java
@@ -3,6 +3,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
@@ -22,6 +25,25 @@ public class JWTAuthenticationFilters extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authHeader =request.getHeader("Authorization");
+        if(authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String token=authHeader.substring(7);
+        String email=jwtServices.extractEmail(token);
 
+        if(email!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserPrincipal userDetails=(UserPrincipal) userDetailService.loadUserByUsername(email);
+            if(jwtServices.validateToken(token,userDetails.getUsername())){
+                UsernamePasswordAuthenticationToken authToken=new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
